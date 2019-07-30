6f3b5f57b5af43f9907bb421b116a7968bff653b
cloudtom <cloudtom@users.noreply.github.com>
2019-07-29 21:49:15 -0700

    Update end.js

--- end.js
+++ end.js
@@ -24,7 +24,7 @@ var noticeimg = document.getElementById("noticeimage")
 
             
             image.onload = function(){
-                noticeimg.width = image.width
+                noticeimg.width = 800
                 noticeimg.height = image.height
                 ncontext.drawImage( image , 0 , 0 )
 
